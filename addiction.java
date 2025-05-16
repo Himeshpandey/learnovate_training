1502af98ae98d9fbf2b67e70ef26738f0d31a4c7
Himeshpandey <himeshpandey513@gmail.com>
2025-05-16 15:07:18 +0530

    done

---
+++ addiction.java
@@ -0,0 +1,13 @@
+
+public class addiction{
+    public static void max(int a, int b){
+        if(a>b){
+            System.out.print(a);
+        }else{
+            System.out.print(b);
+        }
+    }
+    public static void main(String[] args) {
+        max(5,6);
+    }
+}
---
+++ main.java
@@ -0,0 +1,13 @@
+public class main {
+ public static void main(String[] args) {
+   int a=44;
+   int b=20; 
+   System.out.println(a+b);
+   System.out.println(a-b);
+   System.out.println(a%=b);
+   System.out.println(a==b);
+   System.out.println(a>=b);
+   System.out.println(a++);
+   System.out.println(++a);
+ }   
+}
